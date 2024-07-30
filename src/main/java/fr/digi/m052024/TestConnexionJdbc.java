b4925fc00db4159683fa5ab490b5d8ee5cecfc8d
GaelBuenoBarthe <gaelbueno@aol.com>
2024-07-30 14:21:27 +0200

    add .properties file

--- src/main/java/fr/digi/m052024/TestConnexionJdbc.java
+++ src/main/java/fr/digi/m052024/TestConnexionJdbc.java
@@ -3,15 +3,23 @@ package fr.digi.m052024;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
-    private static final String DB_URL = "jdbc:mariadb://localhost:3308/";
-    private static final String DB_USER = "root";
-    private static final String DB_PWD = "714131";
+    private static final String DB_URL ;
+    private static final String DB_USER ;
+    private static final String DB_PWD ;
+
+    static{
+        ResourceBundle bundle = ResourceBundle.getBundle("db");
+        DB_URL = bundle.getString("db.url");
+        DB_USER = bundle.getString("db.user");
+        DB_PWD = bundle.getString("db.pwd");
+    }
 
     public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
-        System.out.println("Connexion établie !");
+        System.out.println("Connexion réussie à : " + connection);
         connection.close();
     }
 
